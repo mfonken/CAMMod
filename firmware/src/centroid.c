70428a025b1821fba07be52b4e682c90473066e6
mfonken <mgfonken@gmail.com>
2016-12-01 14:58:13 -0700

    centroids format/type fix

--- firmware/src/centroid.c
+++ firmware/src/centroid.c
@@ -1,145 +1,32 @@
-/* ************************************************************************** */
-/** Descriptive File Name
-
-  @Company
-    Company Name
-
-  @File Name
-    filename.c
-
-  @Summary
-    Brief description of the file.
-
-  @Description
-    Describe the purpose of this file.
- */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/* ************************************************************************** */
-/* Section: Included Files                                                    */
-/* ************************************************************************** */
-/* ************************************************************************** */
-
-/* This section lists the other files that are included in this file.
- */
-
 #include "centroid.h"
 
-
-/* ************************************************************************** */
-/* ************************************************************************** */
-/* Section: File Scope or Global Data                                         */
-/* ************************************************************************** */
-/* ************************************************************************** */
-
-/*  A brief description of a section can be given directly below the section
-    banner.
- */
-
-/* ************************************************************************** */
-/** Descriptive Data Item Name
-
-  @Summary
-    Brief one-line summary of the data item.
-    
-  @Description
-    Full description, explaining the purpose and usage of data item.
-    <p>
-    Additional description in consecutive paragraphs separated by HTML 
-    paragraph breaks, as necessary.
-    <p>
-    Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
-    
-  @Remarks
-    Any additional remarks
- */
-int global_data;
-
-
-/* ************************************************************************** */
-/* ************************************************************************** */
-// Section: Local Functions                                                   */
-/* ************************************************************************** */
-/* ************************************************************************** */
-
-/*  A brief description of a section can be given directly below the section
-    banner.
- */
-
-/* ************************************************************************** */
-
-
-static void cma(double new_val, double *avg, int num)
+static void cma( double new_val, double *avg, uint16_t num )
 {
-    *avg += (new_val - *avg)/(num + 1);
+    *avg += ( new_val - *avg ) / ( num + 1 );
 }
-/** 
-  @Function
-    int ExampleLocalFunctionName ( int param1, int param2 ) 
-
-  @Summary
-    Brief one-line description of the function.
-
-  @Description
-    Full description, explaining the purpose and usage of the function.
-    <p>
-    Additional description in consecutive paragraphs separated by HTML 
-    paragraph breaks, as necessary.
-    <p>
-    Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
-
-  @Precondition
-    List and describe any required preconditions. If there are no preconditions,
-    enter "None."
-
-  @Parameters
-    @param param1 Describe the first parameter to the function.
-    
-    @param param2 Describe the second parameter to the function.
-
-  @Returns
-    List (if feasible) and describe the return values of the function.
-    <ul>
-      <li>1   Indicates an error occurred
-      <li>0   Indicates an error did not occur
-    </ul>
-
-  @Remarks
-    Describe any special behavior not described above.
-    <p>
-    Any additional remarks.
-
-  @Example
-    @code
-    if(ExampleFunctionName(1, 2) == 0)
-    {
-        return 3;
-    }
- */
-static int getBlobId(int x, int y, int n_c, int *num_blobs)
+uint8_t getBlobId(uint16_t x, uint16_t y, uint16_t n_c, uint8_t num_blobs)
 {
-    int id = -1;                                // Null id (no adjacents)
-    int i, j;
-    for(i = 0; i < *num_blobs; i++)
+    uint8_t id = -1;                                // Null id (no adjacents)
+    uint8_t i, j;
+    for( i = 0; i < *num_blobs; i++ )
     {
         double x_l = centroids.blobs[i].X;                   // Current average
         double n_l = centroids.blobs[i].w_last;              // Last row width
-        if((y - centroids.blobs[i].y_last) <= MAX_GAP)    // Ensure blob hasn't expired
+        if( ( y - centroids.blobs[i].y_last ) <= MAX_GAP )    // Ensure blob hasn't expired
         {
             if( ( ( x + n_c/2 + MAX_GAP) >= ( x_l - n_l/2) ) && // Check overlap of lower bound of blob and upper (with gap tolerance) of new
                 ( ( x - n_c/2 ) <= ( x_l + n_l/2 + MAX_GAP) ) ) // and of upper bound of blob and lower (with gap tolerance) of new
             {
-                if(id == -1)                    // If new blob is not claimed
+                if( id == -1 )                    // If new blob is not claimed
                 {
                     id = i;                     // Claim it under current id
                 }
                 else                            // Otherwise combine
                 {
-                    cma(centroids.blobs[i].X, &centroids.blobs[id].X, 2);
-                    cma(centroids.blobs[i].Y, &centroids.blobs[id].Y, 2);
-                    (*num_blobs)--;             // Decrement blob counter
-                    for(j = i; j < *num_blobs; j++)
+                    cma( centroids.blobs[i].X, &centroids.blobs[id].X, 2 );
+                    cma( centroids.blobs[i].Y, &centroids.blobs[id].Y, 2 );
+                    ( *num_blobs )--;             // Decrement blob counter
+                    for( j = i; j < *num_blobs; j++ )
                     {
                         centroids.blobs[i] = centroids.blobs[i+1];  // and pack blobs down
                     }
@@ -152,60 +39,37 @@ static int getBlobId(int x, int y, int n_c, int *num_blobs)
     return id;                                  // Return id: Valid if claimed, null if not
 }
 
-
-/* ************************************************************************** */
-/* ************************************************************************** */
-// Section: Interface Functions                                               */
-/* ************************************************************************** */
-/* ************************************************************************** */
-
-/*  A brief description of a section can be given directly below the section
-    banner.
- */
-
-// *****************************************************************************
-
-/** 
-  @Function
-    int ExampleInterfaceFunctionName ( int param1, int param2 ) 
-
-  @Summary
-    Brief one-line description of the function.
-
-  @Remarks
-    Refer to the example_file.h interface header for function usage details.
- */
-void getCentroids( uint8_t *image_line, int line_number, int skip )
+void getCentroids( uint8_t *image_line, uint16_t line_number, uint8_t skip )
 {
-    int gap = -1, num_adj = 0, temp_id = 0;   // Global variables
+    uint8_t gap = -1, num_adj = 0, temp_id = 0;   // Global variables
     double a_x_last = 0;                  // Global last X and Y averages
     centroids.numBlobs = 0;
-    int x;
+    uint16_ x;
     for( x = 0; x < CENTROIDS_WIDTH; x += skip )            // Traverse all columns
     {
         if(image_line[x])                             // Check if pixel is on
         { 
             gap = 0;                                // Reset gap counter
-            cma(x,&a_x_last,num_adj);               // Average adjacent pixels
+            cma( x, &a_x_last, num_adj );               // Average adjacent pixels
             num_adj++;                              // Increment adjacent pixels
         }
-        else if(gap != -1)                          // Otherwise, if gap counter is counting (i.e. there was a recent pixel
+        else if( gap != -1 )                          // Otherwise, if gap counter is counting (i.e. there was a recent pixel
         {
             gap++;                                  // Increment the gap counter
-            if(gap ==  MAX_GAP          ||          // If max gap reached
-                 x == (CENTROIDS_HEIGHT - 1))            // or end of row
+            if( gap == MAX_GAP                 ||   // If max gap reached
+                  x == ( CENTROIDS_HEIGHT - 1 ) )   // or end of row
             {                                               // Include last pixel into a blob
-                temp_id = getBlobId(a_x_last, line_number, num_adj, &centroids.numBlobs);   // Get a blob to add to by coordinates and adjacent pixel width
-                if(temp_id == -1)                   // If no blob return
+                temp_id = getBlobId( a_x_last, line_number, num_adj, &centroids.numBlobs );   // Get a blob to add to by coordinates and adjacent pixel width
+                if( temp_id == -1 )                   // If no blob return
                 {
-                    if(centroids.numBlobs == MAX_BLOBS)           // Check if max blobs have already been filled
+                    if( centroids.numBlobs == MAX_BLOBS )           // Check if max blobs have already been filled
                     {
                         return;                   // If so, end and output
                     }
                     temp_id = centroids.numBlobs++;               // Otherwise make a new id for the blob and increment the id counter
                 }
-                cma(a_x_last, &centroids.blobs[temp_id].X, centroids.blobs[temp_id].w_last); // Cumulate the new pixels into the blob's X average
-                cma(line_number, &centroids.blobs[temp_id].Y, centroids.blobs[temp_id].height); // Cumulate the new row into the blob's Y average
+                cma( a_x_last, &centroids.blobs[temp_id].X, centroids.blobs[temp_id].w_last ); // Cumulate the new pixels into the blob's X average
+                cma( line_number, &centroids.blobs[temp_id].Y, centroids.blobs[temp_id].height ); // Cumulate the new row into the blob's Y average
                 centroids.blobs[temp_id].w_last = num_adj;    // Update blob with: New last row width
                 centroids.blobs[temp_id].x_last = a_x_last;   // last row average
                 centroids.blobs[temp_id].height++;            // height
@@ -220,13 +84,9 @@ void getCentroids( uint8_t *image_line, int line_number, int skip )
     }
 }
 
-void initCentroids( int width, int height )
+void initCentroids( uint16_t width, uint16_t height )
 {
     CENTROIDS_WIDTH  = width;
     CENTROIDS_HEIGHT = height;
     centroids.numBlobs = 0;
 }
-
-/* *****************************************************************************
- End of File
- */
--- firmware/src/centroid.h
+++ firmware/src/centroid.h
@@ -1,32 +1,5 @@
-/* ************************************************************************** */
-/** Descriptive File Name
-
-  @Company
-    Company Name
-
-  @File Name
-    filename.h
-
-  @Summary
-    Brief description of the file.
-
-  @Description
-    Describe the purpose of this file.
- */
-/* ************************************************************************** */
-
 #ifndef _EXAMPLE_FILE_NAME_H    /* Guard against multiple inclusion */
 #define _EXAMPLE_FILE_NAME_H
-
-
-/* ************************************************************************** */
-/* ************************************************************************** */
-/* Section: Included Files                                                    */
-/* ************************************************************************** */
-/* ************************************************************************** */
-
-/* This section lists the other files that are included in this file.
- */
 
 #include <stdint.h>
 
@@ -34,150 +7,35 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-
-    /* ************************************************************************** */
-    /* ************************************************************************** */
-    /* Section: Constants                                                         */
-    /* ************************************************************************** */
-    /* ************************************************************************** */
-
-    /*  A brief description of a section can be given directly below the section
-        banner.
-     */
-
-
-    /* ************************************************************************** */
-    /** Descriptive Constant Name
-
-      @Summary
-        Brief one-line summary of the constant.
     
-      @Description
-        Full description, explaining the purpose and usage of the constant.
-        <p>
-        Additional description in consecutive paragraphs separated by HTML 
-        paragraph breaks, as necessary.
-        <p>
-        Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
-    
-      @Remarks
-        Any additional remarks
-     */
 #define MAX_BLOBS       10
 #define MAX_GAP         3
-
-    // *****************************************************************************
-    // *****************************************************************************
-    // Section: Data Types
-    // *****************************************************************************
-    // *****************************************************************************
-
-    /*  A brief description of a section can be given directly below the section
-        banner.
-     */
-
-
-    // *****************************************************************************
-
-    /** Descriptive Data Type Name
-
-      @Summary
-        Brief one-line summary of the data type.
     
-      @Description
-        Full description, explaining the purpose and usage of the data type.
-        <p>
-        Additional description in consecutive paragraphs separated by HTML 
-        paragraph breaks, as necessary.
-        <p>
-        Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
+typedef struct _blob_t
+{
+    double      X;
+    double      Y;
 
-      @Remarks
-        Any additional remarks
-        <p>
-        Describe enumeration elements and structure and union members above each 
-        element or member.
-     */
+    uint16_t    height;
+    uint16_t    w_last;
+    uint16_t    x_last;
+    uint16_t    y_last;
+} blob_t;
 
-    typedef struct _blob_t
-    {
-        double  X;
-        double  Y;
+typedef struct _centroids_t
+{
+    uint8_t     numBlobs;
+    blob_t      blobs[MAX_BLOBS];
+} centroids_t;
 
-        int     height;
-        int     w_last;
-        int     x_last;
-        int     y_last;
-    } blob_t;
+centroids_t centroids;
 
-    typedef struct _centroids_t
-    {
-        int     numBlobs;
-        blob_t  blobs[MAX_BLOBS];
-    } centroids_t;
+uint16_t    CENTROIDS_WIDTH;
+uint16_t    CENTROIDS_HEIGHT;
     
-    centroids_t centroids;
-    
-    int CENTROIDS_WIDTH;
-    int CENTROIDS_HEIGHT;
-    // *****************************************************************************
-    // *****************************************************************************
-    // Section: Interface Functions
-    // *****************************************************************************
-    // *****************************************************************************
-
-    /*  A brief description of a section can be given directly below the section
-        banner.
-     */
-
-    // *****************************************************************************
-    /**
-      @Function
-        int ExampleFunctionName ( int param1, int param2 ) 
-
-      @Summary
-        Brief one-line description of the function.
-
-      @Description
-        Full description, explaining the purpose and usage of the function.
-        <p>
-        Additional description in consecutive paragraphs separated by HTML 
-        paragraph breaks, as necessary.
-        <p>
-        Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
-
-      @Precondition
-        List and describe any required preconditions. If there are no preconditions,
-        enter "None."
-
-      @Parameters
-        @param param1 Describe the first parameter to the function.
-    
-        @param param2 Describe the second parameter to the function.
-
-      @Returns
-        List (if feasible) and describe the return values of the function.
-        <ul>
-          <li>1   Indicates an error occurred
-          <li>0   Indicates an error did not occur
-        </ul>
-
-      @Remarks
-        Describe any special behavior not described above.
-        <p>
-        Any additional remarks.
-
-      @Example
-        @code
-        if(ExampleFunctionName(1, 2) == 0)
-        {
-            return 3;
-        }
-     */
-   
-void getCentroids( uint8_t *image_line, int line_number, int skip );
-void initCentroids( int width, int height );
+uint8_t getBlobId(uint16_t x, uint16_t y, uint16_t n_c, uint8_t num_blobs);
+void    getCentroids( uint8_t *image_line, uint16_t line_number, uint8_t skip );
+void    initCentroids( uint16_t width, uint16_t height );
 
     /* Provide C++ Compatibility */
 #ifdef __cplusplus
@@ -185,7 +43,3 @@ void initCentroids( int width, int height );
 #endif
 
 #endif /* _EXAMPLE_FILE_NAME_H */
-
-/* *****************************************************************************
- End of File
- */
